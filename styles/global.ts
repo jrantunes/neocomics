661c1da8e2b42056534a5cf2739d1a9dd210d69e
José Neto <joseantunes2001@gmail.com>
2022-01-28 10:16:42 -0300

    chore: styled-components setup, global styles, external font setup

---
+++ styles/global.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media(max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media(max-width: 1080px) {
+      font-size: 87.5%;
+    }
+  }
+  
+  body, input, button {
+    font: 400 1rem "Roboto", sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
