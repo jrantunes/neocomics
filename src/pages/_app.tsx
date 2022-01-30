691f0772e528737fe391c55a447ecd4a12125a28
José Neto <joseantunes2001@gmail.com>
2022-01-30 16:24:19 -0300

    refac: moved header and toast component && moved header scrolling logic to his own file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,26 +6,9 @@ import { CartProvider } from '../hooks/useCart'
 import { GlobalStyle } from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    }
-  }, []);
-  
-  function handleScroll() {
-    const position = window.pageYOffset;
-    setScrollPosition(position);
-  }
-  
   return (
     <CartProvider>
-      <Header 
-        scrollPosition={scrollPosition} 
-      />
+      <Header />
       <ToastContainer autoClose={2000} theme='colored' position='bottom-right' />
       <Component {...pageProps} />
       <GlobalStyle />
