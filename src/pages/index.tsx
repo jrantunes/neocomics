5fc400864f44c5db798022e73e2fcb984324408e
José Neto <joseantunes2001@gmail.com>
2022-01-30 10:44:10 -0300

    fix: removed params from services/api to prevent client side api call errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Home, { HomeTemplateProps } from '../templates/Home';
+import md5 from 'js-md5';
+
 import { GetServerSideProps } from 'next';
 import { api } from '../services/api';
 
@@ -7,15 +9,38 @@ export default function Index(props: HomeTemplateProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const { data } = await api.get('/comics');
+  const publicKey = process.env.NEXT_PUBLIC_MARVEL_API_PUBLIC_KEY;
+  const privateKey = process.env.MARVEL_API_PRIVATE_KEY;
+  
+  const ts = Number(new Date());
+  const hash = md5.create();
+  hash.update(ts + privateKey + publicKey);
+  
+  const { data } = await api.get<{ data: HomeTemplateProps }>('/comics', {
+    params: {
+      ts,
+      apikey: publicKey,
+      hash: hash.toString().toLowerCase(),
+    }
+  });
   
   const { total, count, results } = data.data;
+
+  const comicsData = results.map(result => {
+    return {
+      id: result.id,
+      title: result.title,
+      description: result.description,
+      prices: result.prices,
+      thumbnail: result.thumbnail,
+    }
+  });
 
   return {
     props: {
       total,
       count,
-      results
+      results: [ ...comicsData ]
     }
   }
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,18 +1,5 @@
 import axios from 'axios';
-import md5 from 'js-md5';
-
-const publicKey = process.env.MARVEL_API_PUBLIC_KEY;
-const privateKey = process.env.MARVEL_API_PRIVATE_KEY;
-
-const ts = Number(new Date());
-const hash = md5.create();
-hash.update(ts + privateKey + publicKey);
 
 export const api = axios.create({
   baseURL: 'http://gateway.marvel.com/v1/public',
-  params: {
-    ts,
-    apikey: publicKey,
-    hash: hash.toString().toLowerCase(),
-  }
 });
