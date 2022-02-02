c6459a6a774740d7f9693079770696c1c021feb9
José Neto <joseantunes2001@gmail.com>
2022-02-02 14:42:13 -0300

    refac: isolate  ts/hash params in server side call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,19 @@ import Home, { HomeTemplateProps } from '../templates/Home';
 import { GetServerSideProps } from 'next';
 
 import { getComics } from '../hooks/useComics';
+import { params } from '../utils/serverRequestParams';
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const { total, count, results } = await getComics();
+  const { ts, hash } = params;
+
+  const { total, count, results } = await getComics(1, {
+    ts,
+    hash,
+  });
 
   return {
     props: {
