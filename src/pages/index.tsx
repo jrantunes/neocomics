fdc5d366e59d572c3108c0ded0c5c3f99ecc51cc
José Neto <joseantunes2001@gmail.com>
2022-02-03 18:11:37 -0300

    chore: get initial data in server side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
 import Home from '../templates/Home';
+import { GetServerSideProps } from 'next';
+import { getComics } from '../hooks/useComics';
 
-export default function Index() {
-  return <Home />
+import { ApiResponse } from '../types';
+import { getRequestParams } from '../utils/serverRequestParams';
+
+export default function Index(props: ApiResponse) {
+  return <Home {...props} />
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { ts, hash } = getRequestParams(new Date());
+
+  const { results, total, count } = await getComics(1, {
+    ts,
+    hash
+  });
+
+  return {
+    props: {
+      results,
+      total,
+      count,
+    }
+  }
 }
